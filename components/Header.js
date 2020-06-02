7dcd1f3ad60f9e8575d4b7ada5ac0846f09bc395
Imgopz <rrgkmech@gmail.com>
2020-06-02 22:14:06 +0530

    Second

--- components/Header.js
+++ components/Header.js
@@ -13,12 +13,12 @@ const styles = StyleSheet.create({
     header: {
         width: '100%',
         height: 90,
-        paddingTop:36,
+        paddingTop: 36,
         backgroundColor: '#f7287b',
         alignItems: 'center',
         justifyContent: 'center'
     },
-    headerTitle:{
+    headerTitle: {
         color: 'black',
         fontSize: 18
     }
